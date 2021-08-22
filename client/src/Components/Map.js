936fd25fc7f687d544f151164261a705b1ff8a50
LanaTheodory <81506605+LanaTheodory@users.noreply.github.com>
2021-08-22 12:08:06 +0300

    added map location

---
+++ client/src/Components/Map.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const Map = () => {
+    return (
+        <div>
+            <div className="col-md-6">
+                    
+                    <div className="map">
+                        <h2>Our Location:</h2>
+                        <iframe src="https://www.google.com/maps/d/embed?mid=1pRzE-ZS7IaEUPxTg7gn4KZI9LS0-X_bb" width="1200" height="350"></iframe>
+                       
+                      
+                    </div>
+                    {/* <strong>
+                        Palestine
+                        </strong>
+                        <br/> */}
+                        <strong>
+                        Ramallah - Rukab Street
+                    </strong>
+                    <br/>
+                    <strong>
+                        St Andrew's hall
+                    </strong>
+                </div>
+        </div>
+    )
+}
+
+export default Map
