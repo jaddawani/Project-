f3449bfc50c1fd1b631e085c82eee45022360710
jad dawani <dawanijad@gmail.com>
2021-08-29 14:13:19 +0300

    accept

--- client/src/Components/MPHeader.js
+++ client/src/Components/MPHeader.js
@@ -1,8 +1,4 @@
 import React from "react";
-<<<<<<< HEAD
-// import {Navbar, NavDropdown, Nav, Container} from '@material-ui/core'
-import { Button, Navbar, Container, Nav, Form, FormControl, NavDropdown } from "react-bootstrap";
-=======
 // nodejs library that concatenates classes
 import classNames from "classnames";
 // nodejs library to set properties for components
@@ -15,72 +11,9 @@ import IconButton from "@material-ui/core/IconButton";
 import Button from "@material-ui/core/Button";
 import Hidden from "@material-ui/core/Hidden";
 import Drawer from "@material-ui/core/Drawer";
->>>>>>> 5e96287b476a8ebf06c4ab82435cb7d8240c758d
 
 ///////////
 
-<<<<<<< HEAD
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./Styles/MPHeaderStyle.css"
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-} from "react-router-dom";
-
-const MPHeader = () => {
-    return (
-
-        <div>
-            <div className="row">
-                <div >
-                    <Router>
-                        <Navbar bg="dark" variant="dark" expand="lg" sticky="top">
-                            <img
-                                alt=""
-                                src="https://media.discordapp.net/attachments/847780444954296320/880846660450795530/memorable-modified.png"
-                                width="150"
-                                height="150"
-                                className="d-inline-block align-top"
-                            />{' '}
-                            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                            <Navbar.Collapse id="basic-navbar-nav">
-                                <Nav className="mr-auto">
-                                    <Nav.Link href="/">Home</Nav.Link>
-                                    <Nav.Link href="/about-us">Contact Us</Nav.Link>
-                                    <Nav.Link href="/contact-us">About Us</Nav.Link>
-                                    <NavDropdown title="Birthday Party" id="collasible-nav-dropdown">
-                                        <NavDropdown.Item href="#action/3.1">Private Party</NavDropdown.Item>
-                                        <NavDropdown.Item href="#action/3.2">Pricing</NavDropdown.Item>
-                                        <NavDropdown.Item href="#action/3.3">Add-ones</NavDropdown.Item>
-                                        <NavDropdown.Item href="#action/3.4">Food</NavDropdown.Item>
-
-                                    </NavDropdown>
-                                </Nav>
-                                {/* <Form inline>
-                                    <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-                                    <Button variant="outline-success">Search</Button>
-                                    </Form> */}
-                            </Navbar.Collapse>
-                        </Navbar>
-                        {/* <br />
-                            <Switch>
-                                <Route exact path="/">
-                                    <Home />
-                                </Route>
-                                <Route path="/about-us">
-                                    <AboutUs />
-                                </Route>
-                                <Route path="/contact-us">
-                                    <ContactUs />
-                                </Route>
-                            </Switch> */}
-                    </Router>
-                </div>
-            </div>
-        </div>
-
-=======
 import { Navbar, Nav, NavDropdown } from "react-bootstrap";
 
 ///////////////
@@ -256,7 +189,6 @@ export default function Header(props) {
     </AppBar>
   );
 }
->>>>>>> 5e96287b476a8ebf06c4ab82435cb7d8240c758d
 
 Header.defaultProp = {
   color: "white",
